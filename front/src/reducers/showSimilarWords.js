81b56cc9d1150ca9f30264875546aa303ef200af
massongit <suzukimasaya428@gmail.com>
2018-07-30 00:53:55 +0900

    Add information to package.json

--- front/src/reducers/showSimilarWords.js
+++ front/src/reducers/showSimilarWords.js
@@ -2,6 +2,21 @@ import * as types from "../actions/types"
 import {handleActions} from "redux-actions"
 
 /**
+ * similarに適切に要素が含まれているかどうかをチェックする
+ * @param similar {array} similar
+ * @returns {boolean} similarに適切に要素が含まれているかどうか
+ */
+const checkSimilar = (similar) => {
+    for (const s of similar) {
+        if (!s || !s.word || !s.cosine) {
+            return false
+        }
+    }
+
+    return true
+}
+
+/**
  * Word2Vecによる計算結果の表示ActionのReducer
  */
 export default handleActions(
@@ -9,13 +24,7 @@ export default handleActions(
         // Word2Vecによる計算結果の表示Actionのとき
         [types.SHOW_SIMILAR_WORDS]: (state, action) => {
             if (action.payload.words && action.payload.words.similar) {
-                for (const s of action.payload.words.similar) {
-                    if (!s || !s.word || !s.cosine) {
-                        return state
-                    }
-                }
-
-                if (action.payload.keywords && action.payload.words && !action.payload.words.similar.includes(undefined)) {
+                if (checkSimilar(action.payload.words.similar) && action.payload.keywords && action.payload.words && !action.payload.words.similar.includes(undefined)) {
                     return {
                         keywords: action.payload.keywords,
                         words: action.payload.words
