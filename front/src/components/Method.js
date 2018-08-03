0f22787c70228fd5b18f09b3b10a52de6dc0e3e1
massongit <suzukimasaya428@gmail.com>
2018-08-03 15:15:37 +0900

    Separate process to methods

--- front/src/components/Method.js
+++ front/src/components/Method.js
@@ -16,53 +16,63 @@ class Method extends React.Component {
         intl: intlShape.isRequired
     }
 
+    /**
+     * ラジオボタンを描画する
+     * @returns {*} ラジオボタン
+     */
+    renderRadios() {
+        return this.props.methods.map((m, i) => {
+            if (m in general["word_embeddings.methods"]) {
+                return (
+                    <Radio
+                        key={i}
+                        name="method"
+                        onChange={
+                            () => {
+                                this.props.dispatch(setMethod({
+                                    method: m
+                                }))
+                            }
+                        }
+                        inline
+                    >
+                        {general["word_embeddings.methods"][m]}
+                    </Radio>
+                )
+            } else {
+                return undefined
+            }
+        })
+    }
+
+    /**
+     * 手法の説明を描画する
+     * @returns {*} 手法の説明
+     */
+    renderDescription() {
+        if (this.props.method) {
+            return this.props.intl.formatMessage({
+                id: "description." + this.props.method
+            }).split("\n").map((e, i) => (
+                <p
+                    key={i}
+                    style={{
+                        margin: 0
+                    }}
+                >
+                    {e}
+                </p>
+            ))
+        }
+    }
+
     render() {
         return (
             <div>
                 <FormGroup>
-                    {
-                        this.props.methods.map((m, i) => {
-                            if (m in general["word_embeddings.methods"]) {
-                                return (
-                                    <Radio
-                                        key={i}
-                                        name="method"
-                                        onChange={
-                                            () => {
-                                                this.props.dispatch(setMethod({
-                                                    method: m
-                                                }))
-                                            }
-                                        }
-                                        inline
-                                    >
-                                        {general["word_embeddings.methods"][m]}
-                                    </Radio>
-                                )
-                            } else {
-                                return undefined
-                            }
-                        })
-                    }
+                    {this.renderRadios()}
                 </FormGroup>
-                {
-                    (() => {
-                        if (this.props.method) {
-                            return this.props.intl.formatMessage({
-                                id: "description." + this.props.method
-                            }).split("\n").map((e, i) => (
-                                <p
-                                    key={i}
-                                    style={{
-                                        margin: 0
-                                    }}
-                                >
-                                    {e}
-                                </p>
-                            ))
-                        }
-                    })()
-                }
+                {this.renderDescription()}
             </div>
         )
     }
