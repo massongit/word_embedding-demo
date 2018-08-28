74edf312c615137a3a5efc1e4ca8522b4f5b648f
massongit <suzukimasaya428@gmail.com>
2018-08-28 15:33:52 +0900

    Separate process to function

--- front/src/reducers/showSimilarWords.js
+++ front/src/reducers/showSimilarWords.js
@@ -2,23 +2,26 @@ import * as types from "../actions/types"
 import {handleActions} from "redux-actions"
 
 /**
+ * similarに適切に要素が含まれているかどうかをチェックする
+ * @param similar {array} similar
+ * @returns {boolean} similarに適切に要素が含まれているかどうか
+ */
+const checkSimilar = (similar) => {
+    for (const s of similar) {
+        if (!s || !s.word || !s.cosine) {
+            return false
+        }
+    }
+
+    return true
+}
+
+/**
  * similarWordsActionに適切に要素が含まれているかどうかをチェックする
  * @param action {object} similarWordsAction
  * @returns {boolean} similarWordsActionに適切に要素が含まれているかどうか
  */
-export const checkSimilarWordsAction = (action) => {
-    if (action.payload.keywords && action.payload.words && action.payload.words.similar) {
-        for (const s of action.payload.words.similar) {
-            if (!s || !s.word || !s.cosine) {
-                return false
-            }
-        }
-
-        return true
-    } else {
-        return false
-    }
-}
+export const checkSimilarWordsAction = (action) => (action.payload.keywords && action.payload.words && action.payload.words.similar && checkSimilar(action.payload.words.similar))
 
 /**
  * 分散表現による計算結果の表示ActionのReducer
